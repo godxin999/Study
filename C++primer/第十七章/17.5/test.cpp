dcb1bb6ca2fd31dc3cc68564a3795ee432b7aa68
godxin999 <A996570107@163.com>
2023-03-20 00:13:28 +0800

    update

--- C++primer/第十七章/17.5/test.cpp
+++ C++primer/第十七章/17.5/test.cpp
@@ -35,7 +35,7 @@ void test3(){
 	//使用upperbase在16进制中打印大写X
 	cout<<uppercase<<showbase<<"hex: "<<hex<<20<<" "<<1024<<nouppercase<<noshowbase<<dec<<endl;//hex: 0X14 0X400
 }
-//控制浮点数格式
+//控制浮点数精度
 void test4(){
 	//cout.precision()返回当前精度值，浮点数的精度默认为6位
 	cout<<"Precision: "<<cout.precision()
@@ -47,10 +47,79 @@ void test4(){
 	cout<<"Precision: "<<cout.precision()
 		<<", Value: "<<sqrt(2.0)<<endl;//Precision: 3, Value: 1.41
 }
+//控制浮点数格式
+void test5(){
+	cout<<"default format: "<<100*sqrt(2.0)<<endl;//default format: 141.421
+	//scientific操纵符表示使用科学计数法
+	cout<<"scientific: "<<scientific<<100* sqrt(2.0)<<endl;//scientific: 1.414214e+02
+	//fixed操纵符表示使用定点十进制
+	cout<<"fixed decimal: "<<fixed<<100*sqrt(2.0)<<endl;//fixed decimal: 141.421356
+	//hexfloat操纵符强制浮点数使用16进制格式
+	cout<<"hexadecimal: "<<hexfloat<<100*sqrt(2.0)<<endl;//hexadecimal: 0x1.1ad7bc01366b8p+7
+	//defaultfloat操纵符使流恢复默认状态
+	cout<<"use default: "<<defaultfloat<<100*sqrt(2.0)<<endl;//use default: 141.421
+}
+//打印小数点
+void test6(){
+	cout<<10.0<<endl;//10
+	cout<<showpoint<<10.0<<noshowpoint<<endl;//10.0000
+}
+//输出补白
+void test7(){
+	int i=-16;
+	double d=3.14159;
+	//setw表示下一个数字或者字符串使用输出中最少12个位置
+	cout<<"i: "<<setw(12)<<i<<"next col"<<endl;
+	cout<<"d: "<<setw(12)<<d<<"next col"<<endl;
+	cout<<left;//左对齐
+	cout<<"i: "<<setw(12)<<i<<"next col"<<endl;
+	cout<<"d: "<<setw(12)<<d<<"next col"<<endl;
+	cout<<right;//右对齐
+	cout<<"i: "<<setw(12)<<i<<"next col"<<endl;
+	cout<<"d: "<<setw(12)<<d<<"next col"<<endl;
+	cout<<internal;//补白将会在中间
+	cout<<"i: "<<setw(12)<<i<<"next col"<<endl;
+	cout<<"d: "<<setw(12)<<d<<"next col"<<endl;
+	cout<<setfill('#');//设置补白字符为'#'
+	cout<<"i: "<<setw(12)<<i<<"next col"<<endl;
+	cout<<"d: "<<setw(12)<<d<<"next col"<<endl;
+	cout<<setfill(' ');
+/*
+i:          -16next col
+d:      3.14159next col
+i: -16         next col
+d: 3.14159     next col
+i:          -16next col
+d:      3.14159next col
+i: -         16next col
+d:      3.14159next col
+i: -#########16next col
+d: #####3.14159next col
+*/
+}
+//读取空格
+void test8(){
+	cin>>noskipws;//设置cin读取空白符
+	char ch;
+	while(cin>>ch){
+		cout<<ch;
+	}
+	cin>>skipws;//恢复默认状态，丢弃空白符
+}
+//
+void test9(){
+
+}
+
 
 int main(){
 	//test1();
 	//test2();
 	//test3();
-	test4();
+	//test4();
+	//test5();
+	//test6();
+	//test7();
+	//test8();
+	test9();
 }
