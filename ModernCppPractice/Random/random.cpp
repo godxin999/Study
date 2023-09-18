ddc4df52873c6c30cc1f2ece03c67301a009cc5c
godxin999 <A996570107@163.com>
2023-09-18 13:30:28 +0800

    update

--- ModernCppPractice/Random/random.cpp
+++ ModernCppPractice/Random/random.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <algorithm>
+#include <numeric>
 #include <random>
 #include <ctime>
 #include <vector>
@@ -78,9 +79,9 @@ void test8(){
 }
 */
 //对xorshift32进行封装使其支持std::uniform_int_distribution
-struct xorshift32_state{
+struct xorshift32{
 	using result_type=uint32_t;
-	xorshift32_state(size_t seed):seed(static_cast<result_type>(seed?seed:1)){}
+	xorshift32(size_t seed):seed(static_cast<result_type>(seed?seed:1)){}
 	result_type seed=min();
 	constexpr result_type operator()()noexcept{
 		result_type x=seed;
@@ -97,7 +98,7 @@ struct xorshift32_state{
 	}
 };
 void test9(){
-	xorshift32_state rng(std::random_device{}());
+	xorshift32 rng(std::random_device{}());
 	std::uniform_int_distribution<int> uni(0,100);
 	for(int i=0;i<10;++i){
 		std::cout<<uni(rng)<<std::endl;
@@ -108,13 +109,71 @@ void test9(){
 	}
 }
 void test10(){
-	xorshift32_state rng(std::random_device{}());
+	xorshift32 rng(std::random_device{}());
 	std::vector<std::string> vs={"hello","world","good","morning"};
 	std::uniform_int_distribution<int> uni(0,vs.size()-1);
 	for(int i=0;i<10;++i){
 		std::cout<<vs[uni(rng)]<<std::endl;
 	}
 }
+//正态分布
+void test11(){
+	xorshift32 rng{1};
+	std::normal_distribution<double> nd(0,1);//均值为0，标准差为1
+	for(int i=0;i<10;++i){
+		std::cout<<nd(rng)<<std::endl;
+	}
+}
+//向容器中添加随机数
+void test12(){
+	std::vector<int> vi(100);
+	std::generate(vi.begin(),vi.end(),[rng=xorshift32(std::random_device{}()),uni=std::uniform_int_distribution<int>(0,100)]()mutable{
+		return uni(rng);
+	});
+	for(const auto& i:vi){
+		std::cout<<i<<" ";
+	}
+	std::cout<<std::endl;
+}
+void test13(){
+	std::vector<int> vi;
+	std::generate_n(std::back_inserter(vi),100,[rng=xorshift32(std::random_device{}()),uni=std::uniform_int_distribution<int>(0,100)]()mutable{
+		return uni(rng);
+	});
+	for(const auto& i:vi){
+		std::cout<<i<<" ";
+	}
+	std::cout<<std::endl;
+}
+//使用std::iota填充序列 并使用std::shuffle洗牌
+void test14(){
+	std::vector<int> vi(100);
+	std::iota(vi.begin(),vi.end(),0);
+	std::shuffle(vi.begin(),vi.end(),xorshift32(std::random_device{}()));
+	for(const auto& i:vi){
+		std::cout<<i<<" ";
+	}
+	std::cout<<std::endl;
+}
+//模拟抽卡
+void test15(){
+	std::vector<float> p={0.8,0.15,0.038,0.012};
+	std::vector<float> p_scanned;
+	std::inclusive_scan(p.begin(),p.end(),std::back_inserter(p_scanned));//inclusive_scan为并行std算法，且要求符号满足结合律
+	std::vector<std::string> words={"R","SR","SSR","UR"};
+	xorshift32 rng(std::random_device{}());
+	std::uniform_real_distribution<float> unf(0,1);
+	auto getWord=[&]()->std::string{
+		float f=unf(rng);
+		//std::cout<<f<<std::endl;
+		auto it=std::upper_bound(p_scanned.begin(),p_scanned.end(),f);
+		return words[it-p_scanned.begin()];
+	};
+	for(int i=0;i<10;++i){
+		std::cout<<getWord()<<std::endl;
+	}
+}
+
 
 int main(){
 	//test1();
@@ -126,5 +185,10 @@ int main(){
 	//test7();
 	//test8();
 	//test9();
-	test10();
+	//test10();
+	//test11();
+	//test12();
+	//test13();
+	//test14();
+	test15();
 }
