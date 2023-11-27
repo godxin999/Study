1165139f13ddb39fe8e19a28888f8461650d2fac
godxin999 <A996570107@163.com>
2023-11-28 00:47:07 +0800

    update

---
+++ GameEngineArchitecture/Note/6/code/pool_allocator.cpp
@@ -0,0 +1,89 @@
+#include <cstdint>
+#include <utility>
+#include <iostream>
+#include <vector>
+
+template <typename T,std::uint32_t size_bytes=4096>
+class PoolAllocator{
+public:
+    struct MemoryBlock{
+        char data[sizeof(T)];
+        MemoryBlock* next{nullptr};
+    };
+    struct FreeList{
+        MemoryBlock* head;
+        std::uint32_t size{size_bytes/sizeof(MemoryBlock)};
+    };
+public:
+    PoolAllocator(){
+        pool=new MemoryBlock[size_bytes/sizeof(MemoryBlock)];
+        for(int i=0;i<size_bytes/sizeof(MemoryBlock);++i){
+            pool[i].next=free_list.head;
+            free_list.head=&pool[i];
+        }
+    }
+    PoolAllocator(const PoolAllocator&)=delete;
+    PoolAllocator& operator=(const PoolAllocator&)=delete;
+    ~PoolAllocator(){
+        delete[] pool;
+    }
+public:
+    T* allocate(){
+        if(free_list.size==0){
+            return nullptr;
+        }
+        MemoryBlock* block=free_list.head;
+        free_list.head=block->next;
+        --free_list.size;
+        std::cout<<"allocate"<<std::endl;
+        return reinterpret_cast<T*>(block);
+    }
+    void deallocate(T* p){
+        MemoryBlock* block=reinterpret_cast<MemoryBlock*>(p);
+        block->next=free_list.head;
+        free_list.head=block;
+        ++free_list.size;
+        std::cout<<"deallocate"<<std::endl;
+    }
+    template <typename U, typename... Args>
+    void construct(U* p, Args&&... args){
+        new(p) U(std::forward<Args>(args)...);
+        std::cout<<"construct"<<std::endl;
+    }
+    void destroy(T* p){
+        p->~T();
+        std::cout<<"destroy"<<std::endl;
+    }
+private:
+    MemoryBlock* pool{nullptr};
+    FreeList free_list;
+};
+
+struct st{
+    int a{0};
+    int b{0};
+};
+
+
+int main(){
+    PoolAllocator<st,32> pool;
+    std::vector<st*> v;
+    for(int i=0;i<4;++i){
+        st* p=pool.allocate();
+        if(!p){
+            std::cout<<"allocate failed"<<std::endl;
+            break;
+        }
+        pool.construct(p);
+        v.push_back(p);
+    }
+    std::cout<<v.size()<<std::endl;
+    for(auto it=v.begin();it!=v.end();){
+        pool.destroy(*it);
+        pool.deallocate(*it);
+        it=v.erase(it);
+    }
+    std::cout<<v.size()<<std::endl;
+    
+    return 0;
+}
