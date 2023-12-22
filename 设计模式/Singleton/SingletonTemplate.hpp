2b7196bfe87d3f0452d21eed4a1a3bf63993be37
godxin999 <A996570107@163.com>
2023-12-23 00:23:17 +0800

    update

--- 设计模式/Singleton/SingletonTemplate.hpp
+++ 设计模式/Singleton/SingletonTemplate.hpp
@@ -8,26 +8,39 @@ template <typename T>
 class Singleton{
 protected:
     Singleton()=default;
+private:
     Singleton(const Singleton&)=delete;
     Singleton& operator=(const Singleton&)=delete;
+    
     inline static std::shared_ptr<T> _instance=nullptr;
     inline static std::once_flag s_flag;
+
+    template <typename... Args>
+    static std::shared_ptr<T> createInstance(Args&&... args){
+        struct enable_make_shared:public T{
+            enable_make_shared(Args&&... args):T(std::forward<Args>(args)...){}
+        };
+        return std::make_shared<enable_make_shared>(std::forward<Args>(args)...);
+    }
 public:
     void printAddress(){
         std::cout << std::format("Singleton address: {}\n", static_cast<void*>(_instance.get()));
     }
+    
     static std::shared_ptr<T> getInstance(){
         std::call_once(s_flag,[&](){
-            _instance=std::shared_ptr<T>(new T);
+            _instance=createInstance();
         });
         return _instance;
     }
+    
     template <typename... Args>
     static std::shared_ptr<T> Construct(Args&&... args){
         std::call_once(s_flag,[&](){
-            _instance=std::shared_ptr<T>(new T(std::forward<Args>(args)...));
+            _instance=createInstance(std::forward<Args>(args)...);
         });
         return _instance;
     }
+    
     virtual ~Singleton()=default;
 };
